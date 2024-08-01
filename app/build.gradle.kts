c7a63ed3c0ecd62e6b919472ac3f9e34a27ea1b2
user <Ya111ep@mail.ru>
2024-08-01 20:12:32 +0700

    Подключение библиотеки firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -51,6 +52,9 @@ android {
 
 dependencies {
 
+    implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-database")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
     implementation("androidx.activity:activity-compose:1.9.0")
--- app/src/main/java/com/team/ithub/MainActivity.kt
+++ app/src/main/java/com/team/ithub/MainActivity.kt
@@ -3,15 +3,39 @@ package com.team.ithub
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.team.ithub.ui.theme.ItHubTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        auth = Firebase.auth
         setContent {
             ItHubTheme {
 
             }
         }
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+
+        }
+    }
+
+    @Preview
+    @Composable
+    fun HelloCompose(
+        title: String = "Hello compose",
+        content: String = "Text from Jetpack compose",
+    ) {
+        Text(text = title)
+        Text(text = " ")
     }
 }
